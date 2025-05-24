24597739ae3d12dea47cf72ae6fb61a47772e502
Harshana2 <hkarunarathne2000@gmail.com>
2025-05-24 16:42:58 +0530

    Initial commit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 };
 
 export default App;
--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="bg-blue-400 h-16 p-4 text-xl text-blue-950">
+        <ul className="flex justify-end space-x-6">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
