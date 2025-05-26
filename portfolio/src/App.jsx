db8a8aad109f56f7b45efbb3005795e22bd7bc10
Harshana2 <hkarunarathne2000@gmail.com>
2025-05-26 15:41:56 +0530

    design components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <About />
+      <Projects />
+      <Contact />
     </div>
   );
 };
